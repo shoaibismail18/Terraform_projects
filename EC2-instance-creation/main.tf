239af1af87747985fcd3a353c28550a008692898
Shoaib Ismail <shoaibismail18@gmail.com>
2025-09-09 12:08:30 +0000

    fixed tf file errors

--- EC2-instance-creation/main.tf
+++ EC2-instance-creation/main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "example" {
     ami           = "ami-00ca32bbc84273381" #Amazon Linux 2 AMI (HVM), SSD Volume Type
     instance_type = "t2.micro" #Free tier eligible instance type
     subnet_id = "subnet-0ed8d3403d1e0b963" #replace with your subnet id
-    key_name = "test.pem" #replace with your key pair name
+    key_name = "test" #replace with your key pair name
   
 }
